f04b0324ed6f05063d5c187149fc9efa22a5c1a3
Alexander D. Troelsen <lozander@outlook.com>
2022-05-14 03:04:01 +0200

    Refactoring of character entry

--- src/pathtracker/gui/CharacterEntryImpl.ts
+++ src/pathtracker/gui/CharacterEntryImpl.ts
@@ -6,42 +6,63 @@ export class CharacterEntryImpl implements CharacterEntry,CharacterObserver {
     _nameDiv: HTMLDivElement;
     _initDiv: HTMLDivElement;
     constructor(character: Character, tracker: Tracker) {
-        this._entryDiv = document.createElement("div");
+        this._entryDiv = document.createElement('div');
         this._character = character;
-        let initDiv = document.createElement("div");
-        initDiv.textContent = character.initiative + "";
 
-        let nameDiv = document.createElement("div");
-        nameDiv.textContent = character.name;
-
+        const initDiv = this.createInitDiv(character.initiative);
         this.entryDiv.appendChild(initDiv);
-        this.entryDiv.appendChild(nameDiv);
 
-        this.entryDiv.classList.add("character");
-        initDiv.classList.add("init");
-        nameDiv.classList.add("name");
-        if(character.type == CharacterType.ENEMY) this.entryDiv.classList.add("enemy");
-
-        let removeButton = document.createElement('button');
-        removeButton.addEventListener('click', () => {
-            tracker.remove(character.name);
-        });
-
-        removeButton.classList.add('removeButton');
+        const nameDiv = this.createNameDiv(character.name);
+        this.entryDiv.appendChild(nameDiv);        
         
-        let removeButtonText = document.createTextNode('\xD7'); // Adds cross to remove button
-        removeButton.appendChild(removeButtonText);
-        
+        const removeButton = this.createRemoveButton(character.name, tracker);
         this.entryDiv.appendChild(removeButton);
+        
+        this.entryDiv.classList.add('character');
+        if(character.type == CharacterType.ENEMY) this.entryDiv.classList.add('enemy');
         
         this.setInTurn(tracker.characterInTurn);
     }
 
+
+    private createInfoDiv(value: string): HTMLDivElement {
+        const infoDiv = document.createElement('div');
+        const infoText = document.createTextNode(value);
+        infoDiv.appendChild(infoText);
+        return infoDiv;
+    }
+
+    private createInitDiv(initiative: number): HTMLDivElement {
+        const initDiv = this.createInfoDiv(initiative + '')
+        initDiv.classList.add("init");
+        return initDiv;
+    }
+
+    private createNameDiv(name: string): HTMLDivElement {
+        const nameDiv = this.createInfoDiv(name);
+        nameDiv.classList.add("name");
+        return nameDiv;
+    }
+
+    private createRemoveButton(name: string,tracker: Tracker): HTMLButtonElement {
+        const removeButton = document.createElement('button');
+        const removeButtonText = document.createTextNode('\xD7'); // Adds cross to remove button
+        removeButton.appendChild(removeButtonText);
+        
+        removeButton.classList.add('removeButton');
+        
+        removeButton.addEventListener('click', () => {
+            tracker.remove(name);
+        });
+
+        return removeButton;
+    }
+
     setInTurn(inturn: Character) {
         if(this._character === inturn) {
-            this.entryDiv.classList.add("inturn");
+            this.entryDiv.classList.add('inturn');
         } else {
-            this.entryDiv.classList.remove("inturn");
+            this.entryDiv.classList.remove('inturn');
         }
     }
 
