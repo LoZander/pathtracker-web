a7c4279e8cd4943ae48d917ac6cf4dcc43c7bcc4
Alexander D. Troelsen <lozander@outlook.com>
2022-05-14 10:31:45 +0200

    Removed debugging stuff

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ const gui = new GuiImpl(tracker);
 const controller: Controller = new ControllerImpl(tracker);
 tracker.addTrackerObserver(gui);
 
-tracker.addCharacter("Test1", 30, CharacterType.PLAYER);
-tracker.addCharacter("Test2", 10, CharacterType.PLAYER);
-tracker.addCharacter("Test3", 2, CharacterType.ENEMY);
-tracker.addCharacter("Test4", 20, CharacterType.ENEMY);
-tracker.addCharacter("Test5", 23, CharacterType.PLAYER);
-
 window.onload = () => {
     gui.update();
 }
