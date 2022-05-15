3a0b69e9fa355184647ef1781efecc25a6d1bd03
Alexander D. Troelsen <lozander@outlook.com>
2022-05-15 14:25:25 +0200

    Removed redundancyt

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { ControllerImpl } from './pathtracker/gui/ControllerImpl.js';
 import { Controller, Tracker } from './pathtracker/framework/interfaces.js';
 import { GuiImpl } from './pathtracker/gui/GuiImpl.js';
 import { TrackerImpl } from './pathtracker/standard/TrackerImpl.js';
+import { SyncJSONFileManager } from 'pathtracker/standard/SyncJSONFileManager.js';
 
-const tracker: Tracker = new TrackerImpl();
+const tracker: Tracker = new TrackerImpl(new SyncJSONFileManager());
 const gui = new GuiImpl(tracker);
 const controller: Controller = new ControllerImpl(tracker);
 tracker.addTrackerObserver(gui);
